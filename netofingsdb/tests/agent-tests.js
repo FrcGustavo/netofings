369d0430c494422069e1632dedbc4b1938a0af6e
FrcGustavo <hidalgo_fco520@outlook.com>
2020-02-25 16:38:55 -0600

    Implementacion de createOrUpdate

--- netofingsdb/tests/agent-tests.js
+++ netofingsdb/tests/agent-tests.js
@@ -14,16 +14,29 @@ const MetricStub = {
   belongsTo: sinon.spy()
 }
 
-// const single = Object.assign({}, agentFixtures.single)
+const single = Object.assign({}, agentFixtures.single)
 const id = 1
+const uuid = 'yyy-yyy-yyy'
 let AgentStub = null
 let db = null
 let sandbox = null
+const uuidArgs = {
+  where: { uuid }
+}
+
 test.beforeEach(async () => {
   sandbox = sinon.createSandbox()
   AgentStub = {
     hasMany: sandbox.spy()
   }
+
+  // Model update Stub
+  AgentStub.update = sandbox.stub()
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single))
+
+  // Model findOne Stub
+  AgentStub.findOne = sandbox.stub()
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.byUuid(uuid)))
 
   // Model finById Stub
   AgentStub.findById = sandbox.stub()
@@ -59,4 +72,17 @@ test.serial('Agent#findById', async t => {
   t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
 
   t.deepEqual(agent, agentFixtures.findbyId(id), 'Should be the same')
+})
+
+test.serial('Agent#createOrUpdate - exists', async t => {
+  const agent = await db.Agent.createOrUpdate(single)
+
+  t.true(AgentStub.findOne.called, 'findOne should be called on model')
+  t.true(AgentStub.findOne.calledTwice, 'findOne should be called twice')
+  t.true(AgentStub.findOne.calledWith(uuidArgs), 'findOne should be called with uuid args')
+  t.true(AgentStub.update.called, 'agent.update called on model')
+  t.true(AgentStub.update.calledOnce, 'agent.update should be called once')
+  t.true(AgentStub.update.calledWith(single), 'agent.update should be called with specified args')
+
+  t.deepEqual(agent, single, 'agent should be the same')
 })
