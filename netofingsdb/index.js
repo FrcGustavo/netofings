72815bbf224e28f60d4f534fc5ecfeb4ff8cacb8
FrcGustavo <hidalgo_fco520@outlook.com>
2020-02-25 15:54:37 -0600

    Creación de fixtures y definición básica del servicio de Agent

--- netofingsdb/index.js
+++ netofingsdb/index.js
@@ -3,6 +3,7 @@
 const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
+const setupAgent = require('./lib/agent')
 const defaults = require('defaults')
 module.exports = async function (config) {
   config = defaults(config, {
@@ -28,7 +29,7 @@ module.exports = async function (config) {
     await sequelize.sync({ force: true })
   }
 
-  const Agent = {}
+  const Agent = setupAgent(AgentModel)
   const Metric = {}
 
   return { Agent, Metric }
--- netofingsdb/tests/agent-tests.js
+++ netofingsdb/tests/agent-tests.js
@@ -1,43 +1,52 @@
 'use strict'
 
 const test = require('ava')
-const sinon = require('sinon');
+const sinon = require('sinon')
 const proxyquire = require('proxyquire')
 
+const agentFixtures = require('./fixtures/agent')
+
 const config = {
-    logging: function() {}
+  logging: function () {}
 }
 
-let MetricStub = {
-    belongsTo: sinon.spy()
+const MetricStub = {
+  belongsTo: sinon.spy()
 }
 
-let AgentStub = null;
+// const single = Object.assign({}, agentFixtures.single)
+const id = 1
+let AgentStub = null
 let db = null
 let sandbox = null
-test.beforeEach(async() => {
-    sandbox = sinon.createSandbox()
-    AgentStub = {
-        hasMany: sandbox.spy()
-    }
-    const setupDatabase = proxyquire('../index', {
-        './models/agent': () => AgentStub,
-        './models/metric': () => MetricStub
-    })
-    db = await setupDatabase(config)
+test.beforeEach(async () => {
+  sandbox = sinon.createSandbox()
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+  const setupDatabase = proxyquire('../index', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+  db = await setupDatabase(config)
 })
 
 test.afterEach(() => {
-    sandbox && sinon.resetHistory()
+  sandbox && sinon.resetHistory()
 })
 
 test('Agent', t => {
-    t.truthy(db.Agent, 'Agent service should exist')
+  t.truthy(db.Agent, 'Agent service should exist')
 })
 
 test.serial('Setup', t => {
-    t.true(AgentStub.hasMany.called, 'AgenModel.hasMany was executed')
-    t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
-    t.true(MetricStub.belongsTo.called, 'MetricModel was executed')
-    t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+  t.true(AgentStub.hasMany.called, 'AgenModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+})
+
+test.serial('Agent#findById', async t => {
+  const agent = await db.Agent.findById(id)
+  t.deepEqual(agent, agentFixtures.findbyId(id), 'Should be the same')
 })
