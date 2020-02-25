eb7da4d147cb9e8a351f9f397046d5f6ee88cdf3
FrcGustavo <hidalgo_fco520@outlook.com>
2020-02-25 00:36:51 -0600

    Creando una advertencia sobre el borrado de base de datos

--- netofingsdb/setup.js
+++ netofingsdb/setup.js
@@ -1,29 +1,43 @@
 'use strict'
 
 const debug = require('debug')('netofings:db:setup')
+const inquirer = require('inquirer');
+const chalk = require('chalk')
 const db = require('./index')
 
-async function setup () {
-  const config = {
-    database: process.env.DB_NAME || 'netofings',
-    username: process.env.DB_USER || 'gusi',
-    password: process.env.DB_PASS || '123456',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
-    logging: s => debug(s),
-    setup: true
-  }
+const prompt = inquirer.createPromptModule();
 
-  await db(config).catch(handleFatalError)
+async function setup() {
+    const answer = await prompt({
+        type: 'confirm',
+        name: 'setup',
+        message: 'This will destroy your database, are you sure?'
+    })
 
-  console.log('Success!')
-  process.exit(1)
+    if (!answer.setup) {
+        return console.log('Nothing happened :)')
+    }
+
+    const config = {
+        database: process.env.DB_NAME || 'netofings',
+        username: process.env.DB_USER || 'gusi',
+        password: process.env.DB_PASS || '123456',
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'postgres',
+        logging: s => debug(s),
+        setup: true
+    }
+
+    await db(config).catch(handleFatalError)
+
+    console.log('Success!')
+    process.exit(0)
 }
 
-function handleFatalError (error) {
-  console.error(error.message)
-  console.error(error.stack)
-  process.exit(1)
+function handleFatalError(error) {
+    console.error(`${chalk.red('[fatal error]')} ${error.message}`)
+    console.error(error.stack)
+    process.exit(1)
 }
 
 setup()
