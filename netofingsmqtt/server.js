1e34c654b29bcf9257172989ccc6fb5b9aa1e14a
FrcGustavo <hidalgo_fco520@outlook.com>
2020-02-25 23:52:54 -0600

    Como integrar el servidor MQTT con la base de datos

--- netofingsmqtt/server.js
+++ netofingsmqtt/server.js
@@ -4,11 +4,21 @@ const debug = require('debug')('netofings:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('netofingsdb')
 
 const backend = {
   type: 'redis',
   redis,
   return_buffers: true
+}
+
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'platzi',
+  password: process.env.DB_PASS || 'platzi',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: s => debug(s)
 }
 
 const settings = {
@@ -17,6 +27,8 @@ const settings = {
 }
 
 const server = new mosca.Server(settings)
+
+let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
@@ -31,7 +43,11 @@ server.on('published', (packet, client) => {
   debug(`Payload: ${packet.payload}`)
 })
 
-server.on('ready', () => {
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+
+  Agent = services.Agent
+  Metric = services.Metric
   console.log(`${chalk.green('[netofings-mqtt]')} server is running`)
 })
 
