ed61fb986d567e45a25a652869317b2eb7e06f1e
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-03 17:53:55 -0600

    Custom Agent Even Emitter

---
+++ netofingsagent/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const EventEmitter = require('events');
+
+class NetofingsAgent extends EventEmitter {
+    constructor() {
+        super()
+    }
+}
+
+module.exports = NetofingsAgent;
