47bc2fc0371bed27861175014d991b99defaabf4
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-03 16:11:03 -0600

    Fixing conectio to db

--- netofingsmqtt/server.js
+++ netofingsmqtt/server.js
@@ -21,8 +21,8 @@ const settings = {
 
 const config = {
   database: process.env.DB_NAME || 'netofings',
-  username: process.env.DB_USER || 'platzi',
-  password: process.env.DB_PASS || 'platzi',
+  username: process.env.DB_USER || 'gusi',
+  password: process.env.DB_PASS || '123456',
   host: process.env.DB_HOST || 'localhost',
   dialect: 'postgres',
   logging: s => debug(s)
