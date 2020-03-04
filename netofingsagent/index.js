db31c60964937d43fc854f8726a7b25a50048344
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-03 20:18:22 -0600

    Definiendo timer de ejecicion

--- netofingsagent/index.js
+++ netofingsagent/index.js
@@ -1,11 +1,33 @@
 'use strict'
 
-const EventEmitter = require('events');
+const EventEmitter = require('events')
 
 class NetofingsAgent extends EventEmitter {
-    constructor() {
-        super()
+  constructor (opts) {
+    super()
+    this._options = opts
+    this._started = false
+    this._timer = null
+  }
+
+  connect () {
+    if (!this._started) {
+      this._started = true
+      this.emit('connected')
+      const opts = this._options
+      this._timer = setInterval(() => {
+        this.emit('agent/message', 'this is a message')
+      }, opts.interval)
     }
+  }
+
+  disconnect () {
+    if (this._started) {
+      clearInterval(this._timer)
+      this._started = false
+      this.emit('disconnectd')
+    }
+  }
 }
 
-module.exports = NetofingsAgent;
+module.exports = NetofingsAgent
