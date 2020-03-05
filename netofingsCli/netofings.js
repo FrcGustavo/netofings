e8077a11cbb78f1210a059734cf7241a23b7328b
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-05 15:20:50 -0600

    Integracion de agente de monitoreo

--- netofingsCli/netofings.js
+++ netofingsCli/netofings.js
@@ -6,8 +6,13 @@
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const NetofingsAgent = require('netofingsagent')
 
+const agent = new NetofingsAgent()
 const screen = blessed.screen()
+
+const agents = new Map()
+const agentMetrics = new Map()
 
 const grid = new contrib.grid({
   rows: 1,
@@ -26,8 +31,39 @@ const line = grid.set(0, 1, 1, 3, contrib.line, {
   xPadding: 5
 })
 
+agent.on('agent/connected', payload => {
+  const { uuid } = payload.agent
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentMetrics.set(uuid, {})
+  }
+
+  renderData()
+})
+
+function renderData () {
+  const treeData = {}
+
+  for (const [uuid, val] of agents) {
+    const title = ` ${val.name} - (${val.pid})`
+    treeData[title] = {
+      uuid,
+      agent: true,
+      children: {}
+    }
+  }
+
+  tree.setData({
+    extended: true,
+    children: treeData
+  })
+  screen.render()
+}
+
 screen.key(['escape', 'q', 'C-c'], (ch, key) => {
   process.exit(0)
 })
 
+agent.connect()
 screen.render()
