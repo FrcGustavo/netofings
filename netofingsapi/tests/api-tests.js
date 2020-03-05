4d842c789c1262375e0abc61284e0f8ad19c7b02
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-04 19:10:05 -0600

    Modificando las pruebas de integracion

--- netofingsapi/tests/api-tests.js
+++ netofingsapi/tests/api-tests.js
@@ -1,15 +1,20 @@
 'use strict'
 
 const test = require('ava')
+const util = require('util')
 const request = require('supertest')
 const sinon = require('sinon')
 const proxyquire = require('proxyquire')
 
+const config = require('../config')
 const agentFixtures = require('./fixtures/agent')
+const auth = require('../auth')
+const sign = util.promisify(auth.sign)
 
 let sandbox = null
 let server = null
 let dbStub = null
+let token = null
 const AgentStub = {}
 const MetricStub = {}
 
@@ -24,6 +29,7 @@ test.beforeEach(async () => {
 
   AgentStub.findConnected = sandbox.stub()
   AgentStub.findConnected.returns(Promise.resolve(agentFixtures.connected))
+  token = await sign({ admin: true, username: 'platzi' }, config.auth.secret)
 
   const api = proxyquire('../api', {
     netofingsdb: dbStub
@@ -41,6 +47,7 @@ test.afterEach(() => {
 test.serial.cb('/api/agents', t => {
   request(server)
     .get('/api/agents')
+    .set('Authorization', `Bearer ${token}`)
     .expect(200)
     .expect('Content-Type', /json/)
     .end((err, res) => {
@@ -52,6 +59,7 @@ test.serial.cb('/api/agents', t => {
     })
 })
 
+test.serial.todo('/api/agent/:uuid - no authorized')
 test.serial.todo('/api/agent/:uuid')
 test.serial.todo('/api/agent/:uuid - not found')
 
