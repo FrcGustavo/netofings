f76aaba58ab4355ecfd55ad4079c4a4700c6e4f5
Francisco Gustavo <gustavo@frcgustavo.com>
2026-04-11 16:21:51 -0600

    chore: remove original db project
    Signed-off-by: Francisco Gustavo <gustavo@frcgustavo.com>

--- netofingsdb/src/utils/debug.js
+++
@@ -1,8 +0,0 @@
-const debug = require('debug');
-const config = require('../config');
-
-const info = debug(`${config.logPrefix}:info`);
-const dev = debug(`${config.logPrefix}:dev`);
-const error = debug(`${config.logPrefix}:error`);
-
-module.exports = { info, dev, error };
