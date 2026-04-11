84d6a02f9294bf413fb21ed3af4068f268766866
Francisco Gustavo <gustavo@frcgustavo.com>
2026-04-11 16:20:56 -0600

    chore: remove original api proyect
    Signed-off-by: Francisco Gustavo <gustavo@frcgustavo.com>

--- netofingsapi/src/auth.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const sign = (payload: string | object | Buffer, secret: jwt.Secret, callback: jwt.SignOptions | undefined) => {
-  jwt.sign(payload, secret, callback);
-};
-
-export const verify = (token: string, secret: jwt.Secret, callback: jwt.VerifyOptions & { complete: true }) => {
-  jwt.verify(token, secret, callback);
-};
